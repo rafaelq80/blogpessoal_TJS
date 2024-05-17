58ff0b21657e856fb039505a3a446231cffb647f
Rafael Queiróz <rafaelproinfo@gmail.com>
2024-05-17 12:09:43 -0300

    Blog Pessoal

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,23 @@
+﻿function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4
+            			   bg-indigo-900 text-white'>
+            
+                <div className="container flex justify-between text-lg">
+                    Blog Pessoal
+
+                    <div className='flex gap-4'>
+                        Postagens
+                        Temas
+                        Cadastrar tema
+                        Perfil
+                        Sair
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
